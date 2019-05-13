a460c2bd468b24c88dee82d3694252e8ec5e3133
Kaitlin Park <kaitlinpark@parkaitlin-mac.local>
2019-05-13 13:20:31 -0700

    Commit 2: import react-router and create login component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import * as routes from './constants/routes';
+import{Switch, Route} from 'react-router-dom';
+
+//components
+import Login from './login/login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Switch>
+      <Route exact path={routes.LOGIN} render={() => <Login />} />
+    </Switch>
     </div>
   );
 }
